efc72a9bacdaf836e66bc920ca604389458c44a3
nero <devstory.co.kr@gmail.com>
2024-09-25 14:26:08 +0900

    Updated splash page UI.

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -5,10 +5,8 @@ import 'package:x_pr/app/pages/splash/splash_page_state.dart';
 import 'package:x_pr/app/routes/routes.dart';
 import 'package:x_pr/core/theme/components/anims/anim_trans_opacity.dart';
 import 'package:x_pr/core/theme/components/app_bar/transparent_app_bar.dart';
-import 'package:x_pr/core/theme/components/icons/asset_icon.dart';
 import 'package:x_pr/core/theme/components/image/asset_img.dart';
 import 'package:x_pr/core/theme/res/layout.dart';
-import 'package:x_pr/core/utils/ext/num_ext.dart';
 import 'package:x_pr/core/utils/platform/platform_info.dart';
 import 'package:x_pr/core/view/base_view.dart';
 
@@ -80,43 +78,15 @@ class SplashPage extends StatelessWidget {
 
               /// App Name
               Transform.translate(
-                offset: Offset(0, -context.screen.height * 0.1),
-                child: Stack(
-                  clipBehavior: Clip.none,
-                  alignment: Alignment.center,
-                  children: [
-                    /// Paint mess
-                    Transform.translate(
-                      offset: const Offset(33.5, 0),
-                      child: const OverflowBox(
-                        maxWidth: 620,
-                        maxHeight: 517,
-                        child: AnimTransOpacity(
-                          child: AssetIcon(
-                            'paint_mess',
-                            useIconColor: true,
-                            width: 620,
-                          ),
-                        ),
-                      ),
+                offset: const Offset(33.5, 0),
+                child: OverflowBox(
+                  maxWidth: 659.4,
+                  // maxHeight: 517,
+                  child: AnimTransOpacity(
+                    child: AssetImg(
+                      'splash',
+                      width: 659.4.dw,
                     ),
-
-                    /// App Name
-                    const AnimTransOpacity(
-                      initDelay: Duration(milliseconds: 500),
-                      child: AssetIcon("app_name"),
-                    ),
-                  ],
-                ),
-              ),
-
-              /// Mafia
-              Positioned(
-                bottom: context.safeaAreaPadding.bottom.ifZero(16),
-                child: const AnimTransOpacity(
-                  child: AssetImg(
-                    'drawing_mafia',
-                    height: 280,
                   ),
                 ),
               ),
